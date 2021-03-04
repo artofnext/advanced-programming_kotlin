71beee570fe63ca95f5b929edc38965b7fd4121c
artofnext <novnovalex@gmail.com>
2021-03-04 10:52:57 +0000

    add extension .count({lambda}) function

--- src/main/kotlin/SimpleArrayList.kt
+++ src/main/kotlin/SimpleArrayList.kt
@@ -1,22 +1,53 @@
 fun main() {
+    //test1()
 
+    test2()
+}
+
+fun test1() {
     val list = SimpleArrayList<String>()
+    println("=====Empty list - list.isEmpty")
     println(list.isEmpty)
     list.add("first")
+    println("=====Added 1 element - list.isEmpty")
     println(list.isEmpty)
     list.add("second")
+    println("=====Added 2 elements - list.toString")
     println(list.toString())
+    println("=====Added 2 elements - list.size")
     println(list.size)
+    println("=====Added 2 elements - list.get(1)")
     println(list.get(1))
+    println("=====Added 2 elements - list.contain(\"second\")")
     println(list.contain("second"))
     val list1 = SimpleArrayList<String>()
     list1.add("first list2")
     list1.add("second list2")
+    println("=====New list1 with 2 elements - list1.toString")
     println(list1.toString())
+    println("=====- list1.size")
     println(list1.size)
     val listMerged = list.merge(list1)
+    println("=====val listMerged = list.merge(list1)")
     println(listMerged.toString())
+    println("=====- listMerged.size")
     println(listMerged.size)
+}
+
+fun test2 () {
+    val list = SimpleArrayList<Int>()
+    list.add(12)
+    list.add(122)
+    list.add(2)
+    list.add(2)
+    list.add(2)
+    list.add(2)
+    list.add(48)
+    list.add(49)
+    list.add(38)
+
+    val c = list.count { a: Any? -> a == 2 }
+    println(c)
 }
 
 class SimpleArrayList<T> {
@@ -56,5 +87,13 @@ class SimpleArrayList<T> {
             this.add(list.get(i))
         }
         return this
+    }
+
+    fun count(accept: (Any?) -> Boolean): Int {
+        var counter: Int = 0
+        for (i in 0 until this.size) {
+            if (accept(this.get(i))) counter++
+        }
+        return counter
     }
 }
