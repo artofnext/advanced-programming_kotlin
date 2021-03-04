b02064f6c458a507182b492475b979d467ae5b52
artofnext <novnovalex@gmail.com>
2021-03-04 09:44:44 +0000

    Init commit

---
+++ src/main/kotlin/Collections.kt
@@ -0,0 +1,33 @@
+fun main() {
+
+    val range = (1..10000)
+    val data = List(10000) { range.random() }
+    val timer = System.currentTimeMillis()
+    unicList(data)
+    println(System.currentTimeMillis() - timer)
+
+    val timer1 = System.currentTimeMillis()
+    unicList1(data)
+    println(System.currentTimeMillis() - timer1)
+
+    val set = mutableSetOf(1,2,3)  // hashtable
+    set.contains(3) // constant
+}
+
+fun unicList(list: List<Int>): List<Int> {
+    val result = mutableListOf<Int>()
+    for(e in list) { //linear O(n)
+        if(!result.contains(e)) // linear
+            result.add(e)
+    }
+    return result
+}
+
+fun unicList1(list: List<Int>): Set<Int> { // run 10 - 15 times faster then unicList()
+    val result = mutableSetOf<Int>()
+    for(e in list) { //linear O(n)
+        if(!result.contains(e)) // constant
+            result.add(e)
+    }
+    return result
+}
---
+++ src/main/kotlin/DirScanner.kt
@@ -0,0 +1,84 @@
+import java.io.File
+
+fun main() {
+    val dir = File("C:/Users/Iryna/IdeaProjects/kotlinPA2")
+    val dirList = dirFilesRecursive(dir, includeFile)
+    println("Recursive")
+    dirList.forEach { (println(it)) }
+
+    println("Recursive + High order function")
+    val dirList1 = dirFilesRecursive(dir, includeFileEnds(".xml"))
+    dirList1.forEach { (println(it)) }
+
+    println("Extension + High order function")
+    dir.dirFilesRec(includeFileEnds(".jar")).forEach {
+        println(it)
+    }
+
+    println("Extension1 + High order function")
+    dir.dirFilesRec1(includeFileEnds(".jar")).forEach {
+        println(it)
+    }
+    // println(dirList)
+}
+
+fun dirFiles(dir: File): List<File> {
+    val files = mutableListOf<File>()
+    dir.listFiles().forEach {
+        if (it.isFile)
+            files.add(it)
+    }
+    return files
+}
+
+val includeFile = { file: File -> file.name.endsWith(".kt") }
+
+// High order function returns function
+fun includeFileEnds(ending: String): (File) -> Boolean = { file: File -> file.name.endsWith(ending) }
+
+fun dirFilesRecursive(dir: File, accept: (File) -> Boolean = { true }): List<File> {
+    fun aux(dir: File): List<File> {
+        val files = mutableListOf<File>()
+        if (dir.listFiles() != null) {
+            dir.listFiles().forEach {
+                if (it.isFile && accept(it))
+                    files.add(it)
+                else
+                    files.addAll(aux(it))
+            }
+        }
+        return files
+    }
+    return aux(dir)
+}
+
+// Extension function
+fun File.dirFilesRec(accept: (File) -> Boolean = { true }): List<File> {
+    fun aux(dir: File): List<File> {
+        val files = mutableListOf<File>()
+        if (dir.listFiles() != null) {
+            dir.listFiles().forEach {
+                if (it.isFile && accept(it))
+                    files.add(it)
+                else
+                    files.addAll(aux(it))
+            }
+        }
+        return files
+    }
+    return aux(this)
+}
+
+// Extension function
+fun File.dirFilesRec1(accept: (File) -> Boolean = { true }): List<File> {
+        val files = mutableListOf<File>()
+        if (this.listFiles() != null) {
+            this.listFiles().forEach {
+                if (it.isFile && accept(it))
+                    files.add(it)
+                else
+                    files.addAll(it.dirFilesRec1(accept))
+            }
+        }
+        return files
+}
