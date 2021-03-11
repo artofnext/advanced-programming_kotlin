a8f4fef234fe677789abf4e1bcc189bbe1cd4a95
artofnext <novnovalex@gmail.com>
2021-03-11 12:16:09 +0000

    SQL technology variability - initial

--- src/main/kotlin/SQLGenerator.kt
+++ src/main/kotlin/SQLGenerator.kt
@@ -12,6 +12,7 @@ enum class StudentType {
 }
 
 // 1. CREATE TABLE
+// Return string with SQL request for create table based on class provided as argument
 fun createSQLTable(c: KClass<*>): String {
     var sql = "CREATE TABLE "
     sql += c.simpleName // will be table name
@@ -46,6 +47,7 @@ fun typeCast(t: String): String {
 }
 
 // 2. INSERT INTO
+// Return string with SQL request for populate table with class object data values
 fun insertSQLInto(o: Any): String {
     val clazz: KClass<Any> = o::class as KClass<Any>
 
@@ -61,7 +63,50 @@ fun insertSQLInto(o: Any): String {
             } + ")"
 }
 
+//3. SQL technology variability
 
+interface TypeMapping {
+    fun mapType(c: KClass<*>): String
+    fun mapObject(o: Any?): String
+}
+
+class mySQL: TypeMapping {
+    override fun mapType(c: KClass<*>): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun mapObject(o: Any?): String {
+        TODO("Not yet implemented")
+    }
+}
+
+class SQLGenerator(val typeMapping: TypeMapping) {
+
+    fun createTable(c: KClass<*>): String {
+        return  "CREATE TABLE " +
+                c.simpleName + " (" +
+                c.declaredMemberProperties.joinToString(", ") {
+                            it.name + " " +
+                            typeMapping.mapType(it.returnType as KClass<*>) + " " +
+                            if (it.returnType.isMarkedNullable) "NULL" else "NOT NULL"
+                } + ")"
+    }
+
+    fun insertSQLInto(o: Any): String {
+        TODO("Not yet implemented")
+        val clazz: KClass<Any> = o::class as KClass<Any>
+        // INSERT INTO Student (name, number, type) VALUES ('Alex', 7, 'Doctoral')
+        return "INSERT INTO " +
+                clazz.simpleName + " (" +
+                clazz.declaredMemberProperties.joinToString(", ") { it.name } + ") VALUES (" +
+                clazz.declaredMemberProperties.joinToString(", ") {
+                    val propValue = it.call(o)
+                    if (propValue is String || propValue is Enum<*>)
+                        "'$propValue'"
+                    else propValue.toString()
+                } + ")"
+    }
+}
 
 fun main() {
     val s = Student(7, "Alex", StudentType.Doctoral)
