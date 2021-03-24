0b0d9ac51fd6bba26ada1ea4c933d4f58f96a3b8
artofnext <novnovalex@gmail.com>
2021-03-24 18:39:57 +0000

    add visitor interface

--- src/main/kotlin/VisitorLesson.kt
+++ src/main/kotlin/VisitorLesson.kt
@@ -1,11 +1,26 @@
 
 
-abstract class Element()
+abstract class Element(val name: String) {
 
-class Leaf: Element()
+    abstract fun accept(v: Visitor)
+}
 
-class Composite: Element() {
+interface Visitor {
+    fun visit(c: Composite)
+    fun visit(l: Leaf)
+}
+
+class Leaf(name: String): Element(name) {
+    override fun accept(v: Visitor) {
+        TODO("Not yet implemented")
+    }
+}
+
+class Composite(name: String): Element(name) {
     val children = mutableListOf<Element>()
+    override fun accept(v: Visitor) {
+        TODO("Not yet implemented")
+    }
 }
 
 fun countComposies(c: Composite): Int {
@@ -31,17 +46,17 @@ fun countLeafs(c: Composite): Int {
 }
 
 fun main() {
-    val a = Composite()
-    a.children.add(Leaf())
-    a.children.add(Leaf())
-    a.children.add(Leaf())
-    a.children.add(Leaf())
-    a.children.add(Leaf())
+    val a = Composite("1")
+    a.children.add(Leaf("a"))
+    a.children.add(Leaf("b"))
+    a.children.add(Leaf("c"))
+    a.children.add(Leaf("d"))
+    a.children.add(Leaf("e"))
 
-    val b = Composite()
+    val b = Composite("2")
     a.children.add(b)
 
-    b.children.add(Leaf())
+    b.children.add(Leaf("f"))
 
     println(countLeafs(a))
     println(countComposies(a))
